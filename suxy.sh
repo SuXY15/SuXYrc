41146df0dc416a5dca2878728c00bbc64de6c8b5
SuXY15 <suxy15tsinghua@gmail.com>
2019-04-17 17:23:51 +0800

    not compeletion dot for zsh-autosuggestions

--- suxy.sh
+++ suxy.sh
@@ -10,26 +10,28 @@
 #
 # Copyright 2019 Xingyu Su
 R='\033[0;31m'
-G='033[0;32m'
-B='033[0;34m'
+G='\033[0;32m'
+B='\033[0;34m'
 N='\033[0m'
 
 # = = = = = = = = = = = = = = = =
 # for git
-echo -e "${B}Settings for git config${N}"
+echo -e "${G}Settings for git config${N}"
 git config --global user.name SuXY15
 git config --global user.email suxy15tsinghua@gmail.com
 git config --global credential.helper store
 
 # = = = = = = = = = = = = = = = = 
 # for zsh
-echo -e "${B}Settings for oh-my-zsh and plugins"
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo -e "${G}Settings for oh-my-zsh and plugins${N}"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -q -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+wget https://raw.githubusercontent.com/SuXY15/SuXYrc/master/suxyrc -q -O ${HOME}/.suxyrc
 chsh -s /bin/zsh
-wget http://suxy.info/download/assets/downloads/suxyrc -O ${HOME}/.suxyrc
+echo -e "${B}You need to restart your terminal or machine to enable zsh auto-start."
 
 # = = = = = = = = = = = = = = = = 
 # use tuna mirrors
-python -c "$(wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py -O -)" --global
+echo -e "${G}Settings for oh-my-tuna to use tuna mirrors in global environment${N}"
+python -c "$(wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py -q -O -)" --global
